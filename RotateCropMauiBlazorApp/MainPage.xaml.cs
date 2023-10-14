58ae83f14c8ca0d3be517cb8859ca812cf20eb40
hiroyuki.maruyama <hiroyuki.maruyama@duxi.co.jp>
2023-10-14 16:31:53 +0900

    Add .NET MAUI Blazor project.

---
+++ RotateCropMauiBlazorApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RotateCropMauiBlazorApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RotateCropMauiBlazorApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RotateCropMauiBlazorApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
